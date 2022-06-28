3dca23b0437dd6eb7dc6ab6b0498b4ac463eb7f5
Samuel Palacios <samuelpl0888@gmail.com>
2022-06-28 00:28:18 -0400

    Add functions for generating random number and suit

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 const card = document.querySelector('.card');
 const number = document.querySelector('.number');
 const btn = document.querySelector('.btn');
+
+// Functions
+
+const generateRandomNumber = () => {
+  const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+  const indexNums = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNums];
+};
+
+const generateRandomSuit = () => {
+  const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+  const indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
+};
